21af58ffa83951f58b69086df62af0e0eae5fd67
unknown <farhanseptiansyah2010@gmail.com>
2024-09-04 21:40:39 +0700

    add logo

--- resources/views/layouts/v_deskripsi-trans.blade.php
+++ resources/views/layouts/v_deskripsi-trans.blade.php
@@ -1,6 +1,12 @@
 <div class="page-title">
-    <div class="title-env container mt-5">
-        <h1 class="mb-0 text-uppercase text-center text-warning">DIREKTORI PUTUSAN </h1>
-        <h4 class="description text-center text-warning">PENGADILAN TINGGI AGAMA BANDUNG</h4>
+    <br><br>
+    <div class="container text-center">
+        <div class="row">
+            <div class="col">
+            <img src="{{ asset('template')}}/img/logo-pta.png" width="100" height="120">
+                <h1 class="mb-0 text-uppercase text-center text-light">DIREKTORI PUTUSAN </h1>
+                <h4 class="description text-center text-light">PENGADILAN TINGGI AGAMA BANDUNG</h4>
+            </div>
+        </div>
     </div>
 </div>
